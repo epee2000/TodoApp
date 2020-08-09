341d97cecefa832cbab6d524d11b1213b7db3a40
epee2000 <epee2000@hanmail.net>
2020-08-09 18:14:22 +0900

    인메모리 데이터베이스 프로그래밍 맛보기 2

---
+++ TodoApp/TodoApp.Models/ITodoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(Todo model);   // 입력
+        List<Todo> GetAll();    // 출력
+    }
+}
---
+++ TodoApp/TodoApp.Models/TodoRepositoryInMemory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TodoApp.Models
+{
+    /*
+        void Add(Todo model);   // 입력
+        List<Todo> GetAll();    // 출력
+    */
+    public class TodoRepositoryInMemory : ITodoRepository
+    {
+        private static List<Todo> _todos = new List<Todo>();
+
+        // 생성자
+        public TodoRepositoryInMemory()
+        {
+            _todos = new List<Todo>()
+            {
+                new Todo { Id = 1, Title = "ASP.NET Core 학습", IsDone = false },
+                new Todo { Id = 2, Title = "Blazor 학습", IsDone = false },
+                new Todo { Id = 3, Title = "C# 학습", IsDone = true }
+            };
+        }
+
+        public void Add(Todo model)
+        {
+            model.Id = _todos.Max(t => t.Id) + 1;
+            _todos.Add(model);
+        }
+
+        public List<Todo> GetAll()
+        {
+            return _todos.ToList();
+        }
+
+    }
+}
